8f61e11c131a69e8ec986d871e66ae9356042a17
brody <brodytyler@163.com>
2018-03-06 15:01:16 +0800

    支持32通道读写

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import ( 
 	"fmt"
 	"net"
+	"os"
+	"github.com/cryptix/wav"
+	"os/signal"
+	"syscall"
 )
 
 func GetLocalAddr() {
@@ -22,8 +26,28 @@ func GetLocalAddr() {
 	}
 }
 
-func main() {
+var wrFile = wav.File {
+	SampleRate: 		32000,
+	Channels:		2,
+	SignificantBits:	16,
+}
 
+
+func main() {
+	quit := false
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		for s := range c {
+		switch s {
+		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+			fmt.Println(s)
+			quit = true
+		default:
+			fmt.Println("other", s)
+		}
+		}
+	}()	
 
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP : net.IPv4zero, Port: 2050})
 	if err != nil {
@@ -32,13 +56,49 @@ func main() {
 	}
 	fmt.Printf("Local : <%s> \n", listener.LocalAddr().String())
 
+
 	data := make([]byte, 1500)
+	var wavFile *os.File
+	var wr *wav.Writer
 	for {
 		n, remoteAddr, err := listener.ReadFromUDP(data)
 		if err != nil {
-			fmt.Printf("error during read: %s", err)
+			fmt.Printf("error during read: %s frome<%s>",
+				remoteAddr,  err)
+		}
+		//listener.WriteToUDP(data[:n], remoteAddr)
+
+		if wr == nil {
+			wavFile, err = os.OpenFile(
+				"test.wav", 
+				os.O_WRONLY | os.O_TRUNC | os.O_CREATE,
+				0666,
+			)
+			if err != nil {
+				fmt.Println(err)
+				return 
+			}
+			defer wavFile.Close()
+			
+			if data[0] <= 32 {
+				wrFile.Channels = uint16(data[0])
+			}
+			wr, err = wrFile.NewWriter(wavFile)
+			if err != nil {
+				fmt.Println(err)
+				return 
+			}
+			defer wr.Close()
 		}
 
-		fmt.Printf("<%s> %d\n", remoteAddr, n)
+		err = wr.WriteSample(data[8:n])
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if quit == true {
+			break;
+		}
 	}
+
 }
